29facee8f475dbe4a891d56dea47c885bf4e2668
UMKC CTCFP User <umkc-ctcfpuser@umkc.edu>
2018-04-11 23:01:57 -0500

    Challenge One logic

---
+++ bamazon.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE bamazon;
+USE bamazon;
+
+DROP TABLE products;
+CREATE TABLE products (
+	`item_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `product_name` VARCHAR(255) NOT NULL,
+    `department_name` VARCHAR(255) NOT NULL,
+    `price` DECIMAL(10,2),
+    `stock_quantity` INTEGER(10),
+    PRIMARY KEY (`item_id`)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("running_shoes", "clothing", 150.00, 12);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("dress_shoes", "clothing", 175.00, 8);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("phone", "electronics", 500.00, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("laptop", "electronics", 1200.00, 4);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("tv", "electronics", 800.00, 10);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("polo_shirt", "clothing", 19.99, 75);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("watch", "jewelry", 45.00, 2);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("diamond_ring", "jewelry", 3000.00, 1);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("game_system", "electronics", 399.99, 6);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("tires", "automotive", 125.00, 4);
+
+SELECT * FROM products;
---
+++ bamazonCustomer.js
@@ -0,0 +1,87 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "odessa01",
+    database: "bamazon"
+  });
+  
+  // connect to the mysql server and sql database
+  connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    displayProducts();
+  });
+
+  function displayProducts() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.log("Item ID | Product Name  | Department Name  | Price  | In Stock");
+    for (var i = 0; i < res.length; i++) {
+        console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+    }
+        console.log("-----------------------------------");
+        buyProduct();
+  });
+}
+
+function buyProduct() {
+    inquirer.prompt ([
+        {
+            name: "productId",
+            message: "Please enter Product ID"
+        }, {
+            name: "quantity",
+            message: "How many would you like to purchase?"
+        }
+    ]).then(function(response) {
+        var selectedId = response.productId;
+        var selectedAmt = response.quantity;
+        //take productId and query the mysql table.
+        connection.query("SELECT stock_quantity, price FROM products WHERE item_id=?", selectedId, function(err, res) {
+            if (err) throw err;
+
+            var inStock = res[0].stock_quantity;
+            var price = res[0].price;
+            if (selectedAmt < inStock) {
+                var newQuantity = inStock - selectedAmt;
+                var total = price * selectedAmt;
+                updateTable(selectedId, newQuantity, total);
+            } else {
+                console.log("You tried to purchase more than is in stock.");
+                console.log("-----------------------------------");
+                displayProducts();
+            }
+          });
+    });
+}
+
+function updateTable(itemID, updateQuantity, total) {
+    connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+          {
+            stock_quantity: updateQuantity
+          },
+          {
+            item_id: itemID
+          }
+        ],
+        function(error) {
+          if (error) throw err;
+          console.log("Total Purchase Amount: " + total);
+          connection.end();
+        }
+      );
+}
+
+
+
